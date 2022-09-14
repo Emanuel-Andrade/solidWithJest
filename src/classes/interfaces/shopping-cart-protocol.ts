0e418e431d69fe23a904b6638afaa31bb61b1020
Emanuel-Andrade <emanuel.mtg.96@gmail.com>
2022-09-14 20:29:45 -0300

    some changins in shopcart protocol

--- src/classes/interfaces/shopping-cart-protocol.ts
+++ src/classes/interfaces/shopping-cart-protocol.ts
@@ -2,7 +2,7 @@ import { CartItem } from './cart-item';
 
 export interface ShoppingCartProtocol {
   items: Readonly<CartItem[]>;
-  addItem(item: CartItem): void;
+  addItem([...itens]: CartItem[]): void;
   removeItem(index: number): void;
   total(): number;
   totalWithDicount(): number;
