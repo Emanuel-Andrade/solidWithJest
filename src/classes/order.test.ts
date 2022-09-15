a27ef05d95ff1c8cad76bab997e83131eda85100
Emanuel-Andrade <emanuel.mtg.96@gmail.com>
2022-09-15 20:45:37 -0300

    adding order  test

--- src/classes/order.test.ts
+++ src/classes/order.test.ts
@@ -1,8 +1,25 @@
-import { OrderMock } from '../Mocks/mocks';
+import { OrderMock as sut, ShoppingCartMock } from '../Mocks/mocks';
 describe('Order tester', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
   it('shoul return open before the checkout is called', () => {
-    const spyCheckout = jest.spyOn(OrderMock, 'checkout');
+    const spyCheckout = jest.spyOn(sut, 'checkout');
     expect(spyCheckout).toHaveBeenCalledTimes(0);
-    expect(OrderMock.orderStatus).toEqual('open');
+    expect(sut.orderStatus).toEqual('open');
+  });
+
+  it('should not allow clse cart if its empty', () => {
+    sut.checkout();
+    expect(ShoppingCartMock.isEmpty()).toEqual(true);
+    expect(sut.orderStatus).toEqual('open');
+  });
+
+  it('should close order after checkout is called', () => {
+    ShoppingCartMock.addItem([{ name: 'bread', price: 7.9 }]);
+    const spyCheckout = jest.spyOn(sut, 'checkout');
+    sut.checkout();
+    expect(spyCheckout).toHaveBeenCalled();
+    expect(sut.orderStatus).toEqual('closed');
   });
 });
